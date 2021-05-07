5b0357d90230ca3c7a0bd1151abf6c3f469d0541
melvinj <blytung01@gmail.com>
2021-05-07 15:17:38 +0200

    Basic GDM implementation

--- src/d'Oqeratet/Assets/Scripts/gamedata/GameDataManager.cs
+++ src/d'Oqeratet/Assets/Scripts/gamedata/GameDataManager.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameDataManager : MonoBehaviour
+
+struct Board
 {
-    // Start is called before the first frame update
-    void Start()
+    public int hpTarget { get; set; }
+    public Player[] ps  { get; }
+
+    public int activePlayerIndex;
+
+    public Board(int numPlayers, int hpTarget)
     {
-        
+        Debug.Log("Creating Board Struct");
+        Player[] tempPlayers = new Player[numPlayers];
+
+        for(int i = 0; i < numPlayers; i++) {
+            tempPlayers[i] = new Player();
+        }
+
+        this.hpTarget = hpTarget;
+        this.ps = tempPlayers;
+        this.activePlayerIndex = 0;
+    }
+}
+
+public class GameDataManager : ScriptableObject
+{
+
+    private Board board;
+    
+    //Constructor
+    public GameDataManager Start()
+    {
+        Debug.Log("Constructing GDM");
+        board = new Board(4, 300);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public int nextPlayerTurn() {
+        board.activePlayerIndex++;
+        if(board.activePlayerIndex >= board.ps.Length) {
+            board.activePlayerIndex = 0;
+        }
+        return board.activePlayerIndex;
     }
+
+    public Player getActivePlayer() {
+        return board.ps[board.activePlayerIndex];
+    }
+    
+
+    public Player getPlayerFromIndex(int index) {
+        return board.ps[index];
+    }
+
+
+
+
+
 }
