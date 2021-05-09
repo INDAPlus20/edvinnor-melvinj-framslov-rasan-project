eee98342dc3ce9c882653ec4462c7706c15a90b9
Filip-Ramslov <filip@ramslov.se>
2021-05-09 16:34:53 +0200

    Complete prototype!

--- src/d'Oqeratet/Assets/Scripts/CardInteraction/Card.cs
+++ src/d'Oqeratet/Assets/Scripts/CardInteraction/Card.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Examination Card", menuName = "Examination")]
 public class Card : ScriptableObject
 {
     public Sprite artwork;
-    public int staminaCost; 
+    public int staminaCost;
     public int hpGain;
-    public string name;
+    public new string name;
     public string description;
+
+    public void OnEnable()
+    {
+        if (true)
+        {
+            staminaCost = Random.Range(20, 40);
+            hpGain = Random.Range(12, 18);
+        }
+    }
 
     public Card(bool rand = true){
         artwork = null;
@@ -25,6 +35,7 @@ public class Card : ScriptableObject
     }
 
     public Card(Sprite artwork, int staminaCost, int hpGain, string name, string description){
+        Debug.Log("card created");
         this.artwork = artwork;
         this.staminaCost = staminaCost;
         this.hpGain = hpGain;
