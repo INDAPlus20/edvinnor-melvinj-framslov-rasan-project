8482dbdebc673a090fce0f5f355d625d678c35ce
Rasmus Andersson <70899867+RasmusMEA@users.noreply.github.com>
2021-05-20 15:24:53 +0200

    Updated GDM

--- src/d'Oqeratet/Assets/Scripts/gamedata/GameDataManager.cs
+++ src/d'Oqeratet/Assets/Scripts/gamedata/GameDataManager.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 struct Board
 {
     public int hpTarget; //Graduation goal
     public Player[] ps; //players
     public int activePlayerIndex; //Index of player currently having their turn
-    public List<Card> Assignments; //List of all mandatory assignments
+    public List<AssignmentCard> Assignments; //List of all mandatory assignments
 
-    public Board(int numPlayers, int hpTarget, Player[] temp_input_ps)
+    public Board(int numPlayers, int hpTarget, Player[] temp_input_ps, List<AssignmentCard> assignments)
     {
         Debug.Log("Creating Board Struct");
         //Logs in order to find duplicate constructions
@@ -34,39 +35,43 @@ struct Board
         this.activePlayerIndex = 0;
 
         int hpSum = 0;
-        Assignments = new List<Card>();
-
-        //Generate more assignments until we have an entire deck surpassing the graduation goal
-        while(hpSum < this.hpTarget) 
-        {
-            Card newCard = ScriptableObject.CreateInstance<Card>();
-            Assignments.Add(newCard);
-            hpSum += newCard.hpGain;
-        }
+        Assignments = assignments;
     }
 }
 
 public class GameDataManager : MonoBehaviour
 {
+    private System.Random rng;
 
     //Added through unity GUI
     public Player temp_p1;
     public Player temp_p2;
     public Player temp_p3;
     public Player temp_p4;
+    public GameObject tm;
 
     private Board board;
     private int num_players;
-    
+
+    private GameObject cardPreFab;
+    private GameObject cameraMount;
 
     void Start()
     {
         Debug.Log("Starting GDM");
 
+        rng = new System.Random();
+
+        string path = @"Assets/Jsons/Assignments.json";
+
         Player[] temp_input_ps = new Player[] {temp_p1, temp_p2, temp_p3, temp_p4};
         this.num_players = 4;
 
-        board = new Board(this.num_players, 300, temp_input_ps);
+        board = new Board(this.num_players, 300, temp_input_ps, AssignmentCardLoader.ReadAssignments(path));
+        tm.SetActive(true);
+
+        cardPreFab = Resources.Load<GameObject>("Prefabs/Test Card Disp");
+        cameraMount = GameObject.Find("Camera Mount");
     }
 
     //Set the index of the player currently having their turn
@@ -88,18 +93,66 @@ public class GameDataManager : MonoBehaviour
     }
 
     //Returns the global list of assignments
-    public List<Card> getAssignments() 
+    public List<AssignmentCard> getAssignments() 
     {
-        return board.Assignments;
+        if (board.Assignments == null) {
+            Debug.Log("Assignments null when getting assignments");
+        }
+        try 
+        {
+            List<AssignmentCard> tempassignments;
+            
+            //Deep Copy Magic
+            tempassignments = board.Assignments.ConvertAll(c => ScriptableObject.CreateInstance<AssignmentCard>().InitAndReturnSelf(c.artwork, c.stamina, c.hp, c.name, c.description));
+            Shuffle(tempassignments);
+            return tempassignments;
+        }
+        catch (NullReferenceException) {
+            return null;
+        }
     }
 
     //In future:
     //Will return the next chapter card from the global deck
-    public Card drawChapterCard() 
+    public AssignmentCard drawChapterCard() 
     {
         //@Edvin
         Debug.Log("NOT IN USE");
-        return ScriptableObject.CreateInstance</*Chapter*/Card>();
+        return ScriptableObject.CreateInstance</*Chapter*/AssignmentCard>();
+    }
+
+    //In future:
+    //Will Create a GameObject from players AssignmentCard
+    public AssignmentCard drawPlayerCard() 
+    {
+        Player player = getActivePlayer();
+        AssignmentCard assignment = player.drawCard();
+        Transform playerCard = player.transform.Find("Test Card Disp(Clone)");
+
+        // If no playerCard is found, Create GameObject
+        if (playerCard == null) 
+        {
+            Vector3 pos = new Vector3(0, 1.35f, 0); // Temp
+            playerCard = Instantiate<GameObject>(cardPreFab, pos, cameraMount.transform.rotation * Quaternion.Euler(0, 0, 90)).transform;
+            playerCard.parent = player.transform;
+        }
+
+        // Get text-fields
+        Transform canvas = playerCard.transform.Find("Canvas");
+        Transform title = canvas.transform.Find("Title");
+        Transform sa = canvas.transform.Find("Splash Art");
+        Transform ft = canvas.transform.Find("Flavour Text");
+        Transform hp = canvas.transform.Find("HP");
+        Transform stamina = canvas.transform.Find("Stamina");
+
+        // Update text-fields
+        title.GetComponent<TMPro.TextMeshProUGUI>().text = assignment.name;
+        //sa.GetComponent<Image>().SourceImage = assignment.artwork; // Uncertain of this one
+        ft.GetComponent<TMPro.TextMeshProUGUI>().text = assignment.description;
+        hp.GetComponent<TMPro.TextMeshProUGUI>().text = assignment.hp.ToString();
+        stamina.GetComponent<TMPro.TextMeshProUGUI>().text = Mathf.Abs(assignment.stamina).ToString();
+
+        return assignment;
     }
 
     //Returns an array of player hp-s
@@ -135,6 +188,18 @@ public class GameDataManager : MonoBehaviour
     public void testReference(string reference) 
     {
         Debug.Log("Reference " + reference + " to GDM ok");
+    }
+
+    public void Shuffle<T>(List<T> list)  
+    {
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = rng.Next(n + 1);  
+            T value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
     }
 
 }
