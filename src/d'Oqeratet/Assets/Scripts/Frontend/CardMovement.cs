8737a9888848f4d1c7be8fe903421348facde3b8
Filip-Ramslov <filip@ramslov.se>
2021-05-21 14:21:18 +0200

    Add card motion and stat text

--- src/d'Oqeratet/Assets/Scripts/Frontend/CardMovement.cs
+++ src/d'Oqeratet/Assets/Scripts/Frontend/CardMovement.cs
@@ -6,65 +6,83 @@ using System;
 public class CardMovement : MonoBehaviour
 {
 
-    public string desiredPosition = "draw_deck";
-    private string prevDesiredPosition = "draw_deck";
-    private Transform desiredTransform;
-    private Vector3 desiredTranslation;
+    public string desiredLocation = "draw_deck";
+    private string prevDesiredLocation = "draw_deck";
+    private Vector3 desiredPosition;
     private Vector3 desiredRotation;
     private float speed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        (desiredPosition, desiredRotation) = LocationVectors(desiredLocation);
+        transform.localPosition = desiredPosition;
+        transform.rotation = Quaternion.Euler(desiredRotation.x, desiredRotation.y, desiredRotation.z);
         
+        desiredLocation = "face";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (desiredPosition != prevDesiredPosition) {
-            prevDesiredPosition = desiredPosition;
-            switch (desiredPosition)
-            {
-                case "draw_deck":
-                    desiredTranslation = new Vector3(0.25f, 1.02f, 0.8f);
-                    desiredRotation = new Vector3(270.0f, 270.0f, 0.0f);
-                    break;
-                case "face":
-                    desiredTranslation = new Vector3();
-                    desiredRotation = new Vector3();
-                    break;
-                case "table":
-                    desiredTranslation = new Vector3();
-                    desiredRotation = new Vector3();
-                    break;
-                case "discard_deck":
-                    desiredTranslation = new Vector3();
-                    desiredRotation = new Vector3();
-                    break;
-                default:
-                    Debug.Log("Invalid destination provided!");
-                    break;
-            }
+        if (desiredLocation != prevDesiredLocation) {
+            prevDesiredLocation = desiredLocation;
+            (desiredPosition, desiredRotation) = LocationVectors(desiredLocation);
 
             StopCoroutine("Transform");
             StartCoroutine("Transform");
         }
     }
 
+    private (Vector3, Vector3) LocationVectors(string Location)
+    {
+        switch (Location)
+        {
+            case "draw_deck":
+                return (new Vector3(0.25f, 1.02f, 0.8f), new Vector3(270.0f, 270.0f, 0.0f));
+            case "face":
+                return (new Vector3(0.0f, 1.35f, 0.1f), new Vector3(0.0f, 0.0f, 90.0f));
+            case "table":
+                return (new Vector3(-0.25f, 1.05f, 0.7f), new Vector3(270.0f, 270.0f, 0.0f));
+            case "discard_deck":
+                return (new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f));
+            default:
+                Debug.Log("Invalid destination provided!");
+                return (desiredPosition = new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f));
+        }
+    }
+
     IEnumerator Transform()
     {
-        Vector3 startPosition = transform.position;
-        Vector3 startRotation = transform.eulerAngles;
-        while (transform.position.x < (desiredTranslation.x * Math.Sign(desiredTranslation.x - startPosition.x)))
+        Vector3 startPosition = transform.localPosition;
+        Vector3 startRotation = transform.localEulerAngles;
+        float xRotate = startRotation.x;
+        float yRotate = startRotation.y;
+        float zRotate = startRotation.z;
+        float meter = 0.0f;
+        while (meter < 1.0f)
         {
-            float xStepMove = (desiredTranslation.x - startPosition.x) * speed * Time.deltaTime;
-            float yStepMove = (desiredTranslation.z - startPosition.y) * speed * Time.deltaTime;
-            float zStepMove = (desiredTranslation.x - startPosition.z) * speed * Time.deltaTime;
-            transform.position += new Vector3(xStepMove, yStepMove, zStepMove);
+            meter += speed * Time.deltaTime;
+            float xMove = (desiredPosition.x - startPosition.x) * speed * Time.deltaTime;
+            float yMove = (desiredPosition.y - startPosition.y) * speed * Time.deltaTime;
+            float zMove = (desiredPosition.z - startPosition.z) * speed * Time.deltaTime;
+            xRotate += RotationDiff(desiredRotation.x, startRotation.x) * speed * Time.deltaTime;
+            yRotate += RotationDiff(desiredRotation.y, startRotation.y) * speed * Time.deltaTime;
+            zRotate += RotationDiff(desiredRotation.z, startRotation.z) * speed * Time.deltaTime;
+            transform.localPosition += new Vector3(xMove, yMove, zMove);
+            transform.localRotation = Quaternion.Euler(xRotate, yRotate, zRotate);
             yield return null;
         }
-        transform.position = desiredTranslation;
-        transform.eulerAngles = desiredRotation;
+        transform.localPosition = desiredPosition;
+        transform.localRotation = Quaternion.Euler(desiredRotation.x, desiredRotation.y, desiredRotation.z);
+    }
+
+    float RotationDiff(float desired, float start)
+    {
+        if (Math.Abs(desired - start) >= 180.0f)
+        {
+            return (360.0f - Math.Abs(desired - start)) * -Math.Sign(desired - start);
+        }
+        return desired - start;
     }
 }
